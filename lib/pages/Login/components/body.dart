2ad96387396cea01e02bf408e96a743a40c12100
Juli Taras <julibtaras@gmail.com>
2021-06-16 18:51:49 -0300

    Add body and backgoriund for login page

---
+++ lib/pages/Login/components/body.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_svg/svg.dart';
+import 'package:app_turnar/pages/Login/components/background.dart';
+
+class Body extends StatelessWidget {
+  const Body({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "LOGIN",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: size.height * 0.03),
+            Image.asset(
+              "assets/images/login.png",
+            ),
+            SizedBox(height: size.height * 0.03),
+            SizedBox(height: size.height * 0.03),
+          ],
+        ),
+      ),
+    );
+  }
+}
