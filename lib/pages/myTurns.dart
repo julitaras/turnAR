8c1da44cdb8e06aa9ffe2e86049112cda4bc0511
Juli Taras <julibtaras@gmail.com>
2021-06-22 17:50:10 -0300

    fix merge problems

--- lib/pages/myTurns.dart
+++ lib/pages/myTurns.dart
@@ -5,69 +5,70 @@ class MyTurns extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Mis Turnos"),),
-      body: Container(
-        margin: const EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Card(
-              margin: const EdgeInsets.all(10),
-              child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    const ListTile(
-                        title: Text('PRIMERA DOSIS',
-                          style: TextStyle(fontSize: 17),)
-                    ),
-                    Text("25 de Julio, 08.45hs\n"),
-                    Text("SEDE: Hospital n 47"),
-                  ],
-                ),
-              ),
-            ),
-            Card(
-              margin: const EdgeInsets.all(10),
-              child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    const ListTile(
-                        title: Text('SEGUNDA DOSIS',
-                          style: TextStyle(fontSize: 17),)
-                    ),
-                    Text("25 de Agosto, 08.00hs\n"),
-                    Text("SEDE: Hospital n 47"),
-
-                  ],
-                ),
-              ),
-            ),
-            Card(
-              margin: const EdgeInsets.all(10),
-              child: Padding(
-                padding: EdgeInsets.all(18.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    const ListTile(
-                        title: Text('CHEQUEO',
-                          style: TextStyle(fontSize: 17),)
-                    ),
-                    Text("25 de Noviembre, 10.30hs\n"),
-                    Text("SEDE: Hospital n 33"),
-                  ],
-                ),
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text("Mis Turnos"),
         ),
-
-    )
-    );
+        body: Container(
+          margin: const EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Card(
+                margin: const EdgeInsets.all(10),
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      const ListTile(
+                          title: Text(
+                        'PRIMERA DOSIS',
+                        style: TextStyle(fontSize: 17),
+                      )),
+                      Text("25 de Julio, 08.45hs\n"),
+                      Text("SEDE: Hospital n 47"),
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                margin: const EdgeInsets.all(10),
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      const ListTile(
+                          title: Text(
+                        'SEGUNDA DOSIS',
+                        style: TextStyle(fontSize: 17),
+                      )),
+                      Text("25 de Agosto, 08.00hs\n"),
+                      Text("SEDE: Hospital n 47"),
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                margin: const EdgeInsets.all(10),
+                child: Padding(
+                  padding: EdgeInsets.all(18.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      const ListTile(
+                          title: Text(
+                        'CHEQUEO',
+                        style: TextStyle(fontSize: 17),
+                      )),
+                      Text("25 de Noviembre, 10.30hs\n"),
+                      Text("SEDE: Hospital n 33"),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
-
 }
--- lib/pages/verTurno.dart
+++ lib/pages/verTurno.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import './reservarTurnos.dart';
-import '../main.dart';
+import 'package:app_turnar/pages/reservarTurnos.dart';
+import 'package:app_turnar/pages/home.dart';
 
 class VerTurno extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Ver Turno"),),
+      appBar: AppBar(
+        title: Text("Ver Turno"),
+      ),
       body: Container(
         margin: const EdgeInsets.only(left: 10.0, right: 10.0, top: 30.0),
         child: Column(
@@ -22,7 +24,8 @@ class VerTurno extends StatelessWidget {
               width: double.infinity,
               child: ElevatedButton(
                 onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => ReservarTurnos()));
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => ReservarTurnos()));
                   // TODO: se tendrían que mandar los datos del turno y cambiar el título general de la view
                 },
                 child: const Text('Editar'),
@@ -31,39 +34,45 @@ class VerTurno extends StatelessWidget {
             Container(
               width: double.infinity,
               child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.red[900],
-
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red[900],
+                ),
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (_) => CupertinoAlertDialog(
+                            title: Text('Eliminar Turno'),
+                            content: Text(
+                                '¿Está seguro/a de que desea eliminar su turno?'),
+                            actions: <Widget>[
+                              CupertinoDialogAction(
+                                child: Text('Aceptar'),
+                                isDefaultAction: true,
+                                onPressed: () {
+                                  Navigator.of(context, rootNavigator: true)
+                                      .pop("Discard");
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                      builder: (context) =>
+                                          MyHomePage(title: "TurnAR")));
+                                  // TODO: debe eliminar el turno
+                                },
+                              ),
+                              CupertinoDialogAction(
+                                child: Text('Cancel'),
+                                isDefaultAction: false,
+                                isDestructiveAction: true,
+                                onPressed: () {
+                                  Navigator.of(context, rootNavigator: true)
+                                      .pop("Cancel");
+                                },
+                              ),
+                            ],
+                          ));
+                },
+                child: const Text('Eliminar',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.white)),
               ),
-              onPressed: () {
-                showDialog(
-                    context: context,
-                    builder: (_) => CupertinoAlertDialog(
-                      title: Text('Eliminar Turno'),
-                      content: Text('¿Está seguro/a de que desea eliminar su turno?'),
-                      actions: <Widget>[
-                        CupertinoDialogAction(
-                          child: Text('Aceptar'),
-                          isDefaultAction: true,
-                          onPressed: () {
-                            Navigator.of(context, rootNavigator: true).pop("Discard");
-                            Navigator.of(context).push(MaterialPageRoute(builder: (context) =>  MyHomePage(title: "TurnAR")));
-                            // TODO: debe eliminar el turno
-                          },
-                        ),
-                        CupertinoDialogAction(
-                          child: Text('Cancel'),
-                          isDefaultAction: false,
-                          isDestructiveAction: true,
-                          onPressed: () {
-                            Navigator.of(context, rootNavigator: true).pop("Cancel");
-                          },
-                        ),
-                      ],
-                    ));
-              },
-              child: const Text('Eliminar', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white)),
-            ),
             ),
           ],
         ),
