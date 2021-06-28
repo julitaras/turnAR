a21e33ede072ea4097fd2017dbfc270f9b2c5591
Aldana Rastrelli <aldana.rastrelli@uala.com.ar>
2021-06-28 01:41:19 -0300

    user is available in saveAppointment

--- lib/pages/saveAppointment.dart
+++ lib/pages/saveAppointment.dart
@@ -2,195 +2,206 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import '../api/calendarClient.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class TakeTurn extends StatelessWidget {
+class SetAppointment extends StatefulWidget {
+  SetAppointment({Key? key, required User user})
+      : _user = user,
+        super(key: key);
+
+  final User _user;
+
   @override
-  Widget build(BuildContext context) {
-    return TakeTurnPage();
+  _SetAppointmentState createState() => _SetAppointmentState();
+}
+
+class _SetAppointmentState extends State<SetAppointment> {
+  late User _user;
+
+      DateTime pickedDate = DateTime.now();
+    TimeOfDay pickedTime = TimeOfDay.now();
+    GlobalKey formKey = GlobalKey<FormState>();
+    TextEditingController emailController = TextEditingController();
+    List<String> sites = [
+      'Hospital de Clínicas',
+      'Hospital Fernández',
+      'Hospital Garrahan',
+      'Hospital Rivadavia'
+    ];
+    String selectedSite = 'Hospital Rivadavia';
+    String emailValue = '';
+    CalendarClient calendarClient = CalendarClient();
+
+  @override
+  void initState() {
+    _user = widget._user;
+
+    super.initState();
+    emailController.text = _user.email!;
   }
-}
-
-class TakeTurnPage extends StatefulWidget {
-  @override
-  _TakeTurnPageState createState() => _TakeTurnPageState();
-}
-
-class _TakeTurnPageState extends State<TakeTurnPage> {
-  DateTime pickedDate = DateTime.now();
-  TimeOfDay pickedTime = TimeOfDay.now();
-  GlobalKey formKey = GlobalKey<FormState>();
-  TextEditingController emailController = TextEditingController();
-  List<String> sites = [
-    'Hospital de Clínicas',
-    'Hospital Fernández',
-    'Hospital Garrahan',
-    'Hospital Rivadavia'
-  ];
-  String selectedSite = 'Hospital Rivadavia';
-  String emailValue = '';
-  CalendarClient calendarClient = CalendarClient();
 
   @override
   Widget build(BuildContext context) {
     TextStyle? textStyle = Theme.of(context).textTheme.headline6;
     return Scaffold(
-        appBar: AppBar(title: const Text("Reservar Turno")),
-    body: Container(
-        margin: const EdgeInsets.only(left: 10.0, right: 10.0, top: 30.0),
-        child: Form(
-                    key: formKey,
-                    child: Column(
+        appBar: AppBar(
+          title: Text("Reservar Turno"),
+        ),
+        body: Container(
+            margin: const EdgeInsets.only(left: 10.0, right: 10.0, top: 30.0),
+            child: Form(
+                key: formKey,
+                child: Column(children: <Widget>[
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                      Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Expanded(
-                                    child: TextFormField(
-                                        controller: emailController,
-                                        onSaved: (value) {
-                                          emailValue = value!;
-                                        },
-                                        decoration: InputDecoration(
-                                          labelText: "Email",
-                                          labelStyle: textStyle,
-                                          hintText: 'Ingrese su Email',
-                                          border: OutlineInputBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(10),
-                                          ),
-                                          prefixIcon: Icon(Icons.mail),
-                                        ),
-                                        keyboardType:
-                                            TextInputType.emailAddress,
-                                        autofillHints: [AutofillHints.email],
-                                        validator: (value) {
-                                          if (value == null) {
-                                            return "Falta completar el Email";
-                                          }
-                                        }))
-                              ]),
-                      Container(
-                          margin: EdgeInsets.only(top: 30.0),
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: <Widget>[
-                                Text("Elegí Tu Sede:",
-                                textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                            fontSize: 18,
-                                            fontWeight: FontWeight.bold)),
-                              ])),
-                              Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Expanded(
-                                    child: DropdownButton(
-                                  value: selectedSite,
-                                  icon: const Icon(Icons.arrow_drop_down),
-                                  iconSize: 16,
-                                  elevation: 16,
-                                  style: const TextStyle(color: Colors.green),
-                                  underline: Container(
-                                    height: 2,
-                                    color: Colors.green,
+                        Expanded(
+                            child: TextFormField(
+                                controller: emailController,
+                                onSaved: (value) {
+                                  emailValue = value!;
+                                },
+                                decoration: InputDecoration(
+                                  labelText: "Email",
+                                  labelStyle: textStyle,
+                                  hintText: 'Ingrese su Email',
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
                                   ),
-                                  onChanged: (String? newValue) {
-                                    setState(() {
-                                      selectedSite = newValue!;
-                                    });
-                                  },
-                                  items: sites.map((String value) {
-                                    return DropdownMenuItem<String>(
-                                      value: value,
-                                      child: Text(value,
-                                          style: TextStyle(fontSize: 18)),
-                                    );
-                                  }).toList(),
-                                ))
-                              ]),
-                      Container(
-                          margin: EdgeInsets.only(top: 30.0),
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: <Widget>[
-                                Text("Elegí la Fecha:",
+                                  prefixIcon: Icon(Icons.mail),
+                                ),
+                                keyboardType: TextInputType.emailAddress,
+                                autofillHints: [AutofillHints.email],
+                                validator: (value) {
+                                  if (value == null) {
+                                    return "Falta completar el Email";
+                                  }
+                                }))
+                      ]),
+                  Container(
+                      margin: EdgeInsets.only(top: 30.0),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Text("Elegí Tu Sede:",
                                 textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                            fontSize: 18,
-                                            fontWeight: FontWeight.bold)),
-                              ])),
-                      Container(
-                          margin: EdgeInsets.only(top: 10.0),
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: <Widget>[Expanded(
-                                    child: Card(
-                                  shadowColor: Colors.lightGreen,
-                                  child: ListTile(
-                                    title: Text(
-                                        "${pickedDate.day}/${pickedDate.month}/${pickedDate.year}",
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                            fontSize: 18, color: Colors.green)),
-                                    trailing: Icon(Icons.arrow_drop_down),
-                                    onTap: _pickDate,
-                                  ),
-                                ))          
-                    ])),
-                    Container(
-                          margin: EdgeInsets.only(top: 30.0),
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: <Widget>[
-                                Text("Elegí la Hora:",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold)),
+                          ])),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Expanded(
+                            child: DropdownButton(
+                          value: selectedSite,
+                          icon: const Icon(Icons.arrow_drop_down),
+                          iconSize: 16,
+                          elevation: 16,
+                          style: const TextStyle(color: Colors.green),
+                          underline: Container(
+                            height: 2,
+                            color: Colors.green,
+                          ),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              selectedSite = newValue!;
+                            });
+                          },
+                          items: sites.map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child:
+                                  Text(value, style: TextStyle(fontSize: 18)),
+                            );
+                          }).toList(),
+                        ))
+                      ]),
+                  Container(
+                      margin: EdgeInsets.only(top: 30.0),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Text("Elegí la Fecha:",
                                 textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                            fontSize: 18,
-                                            fontWeight: FontWeight.bold)),
-                              ])),
-                              Container(
-                          margin: EdgeInsets.only(top: 10.0),
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: <Widget>[Expanded(
-                                    child: Card(
-                                  shadowColor: Colors.lightGreen,
-                                  child: ListTile(
-                                    title: Text(
-                                        "${pickedDate.day}/${pickedDate.month}/${pickedDate.year}",
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                            fontSize: 18, color: Colors.green)),
-                                    trailing: Icon(Icons.arrow_drop_down),
-                                    onTap: _pickDate,
-                                  ),
-                                ))          
-                    ])),
-                    Container(
-                          margin: EdgeInsets.only(top: 30.0),
-                          width: double.infinity,
-                          child: ElevatedButton(
-                              child: Text("Guardar",
-                                  style: TextStyle(
-                                      fontSize: 17,
-                                      fontWeight: FontWeight.bold)),
-                              onPressed: () => { /* TODO debe guardar en la base el turno
-                              */})),
-                    // TODO el boton de Calendar debe ir en VerTurno
-                    Container(
-                          margin: EdgeInsets.only(top: 10.0),
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),),
-                              child: Text("Agregar a Google Calendar",
-                                  style: TextStyle(
-                                      fontSize: 17,
-                                      fontWeight: FontWeight.bold,)),
-                              onPressed: () => calendarClient.insert(pickedDate,
-                                  pickedDate.add(Duration(minutes: 30)))))
-                    ])
-                    ))
-    );
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold)),
+                          ])),
+                  Container(
+                      margin: EdgeInsets.only(top: 10.0),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Expanded(
+                                child: Card(
+                              shadowColor: Colors.lightGreen,
+                              child: ListTile(
+                                title: Text(
+                                    "${pickedDate.day}/${pickedDate.month}/${pickedDate.year}",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        fontSize: 18, color: Colors.green)),
+                                trailing: Icon(Icons.arrow_drop_down),
+                                onTap: _pickDate,
+                              ),
+                            ))
+                          ])),
+                  Container(
+                      margin: EdgeInsets.only(top: 30.0),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Text("Elegí la Hora:",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold)),
+                          ])),
+                  Container(
+                      margin: EdgeInsets.only(top: 10.0),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Expanded(
+                                child: Card(
+                              shadowColor: Colors.lightGreen,
+                              child: ListTile(
+                                title: Text(
+                                    "${pickedDate.day}/${pickedDate.month}/${pickedDate.year}",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        fontSize: 18, color: Colors.green)),
+                                trailing: Icon(Icons.arrow_drop_down),
+                                onTap: _pickDate,
+                              ),
+                            ))
+                          ])),
+                  Container(
+                      margin: EdgeInsets.only(top: 30.0),
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          child: Text("Guardar",
+                              style: TextStyle(
+                                  fontSize: 17, fontWeight: FontWeight.bold)),
+                          onPressed: () => {
+                                /* TODO debe guardar en la base el turno
+                              */
+                              })),
+                  // TODO el boton de Calendar debe ir en VerTurno
+                  Container(
+                      margin: EdgeInsets.only(top: 10.0),
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.blue),
+                          ),
+                          child: Text("Agregar a Google Calendar",
+                              style: TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold,
+                              )),
+                          onPressed: () => calendarClient.insert(pickedDate,
+                              pickedDate.add(Duration(minutes: 30)))))
+                ]))));
   }
 
   _pickDate() async {
