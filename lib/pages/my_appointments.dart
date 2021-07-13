6abe6738974bb08c79cdd7396ed2091029012da8
Juli Taras <julibtaras@gmail.com>
2021-07-13 10:35:04 -0300

    Tryig to list convert to Cards. Fail

--- lib/pages/my_appointments.dart
+++ lib/pages/my_appointments.dart
@@ -1,57 +1,26 @@
-import 'package:app_turnar/domain/appointment.dart';
-import 'package:app_turnar/pages/show_appointment.dart';
 import 'package:app_turnar/services/appointment_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class MyAppointments extends StatelessWidget {
+class MyAppointments extends StatefulWidget {
   MyAppointments({Key? key, required User user})
       : _user = user,
         super(key: key);
 
   final User _user;
 
-  //TODO deberia mostrar los turnos que trae de la DB
-  final Appointment appointment1 = Appointment(
-      null,
-      new DateTime.utc(2021, 7, 23),
-      new TimeOfDay(hour: 15, minute: 0),
-      "Primera Dosis".toUpperCase(),
-      'Hospital de Clínicas');
-  final Appointment appointment2 = Appointment(
-      null,
-      new DateTime.utc(2021, 8, 25),
-      new TimeOfDay(hour: 15, minute: 0),
-      "Segunda Dosis".toUpperCase(),
-      'Hospital de Clínicas');
-  final Appointment appointment3 = Appointment(
-      null,
-      new DateTime.utc(2021, 9, 28),
-      new TimeOfDay(hour: 15, minute: 0),
-      "Chequeo".toUpperCase(),
-      'Hospital de Clínicas');
+  @override
+  _MyAppointmentsState createState() => _MyAppointmentsState();
+}
 
-  //TODO deberia mostrar los turnos que trae de la DB
-/*  showpics() {
-    return FutureBuilder(
-        future: _loadAppointments(),
-        builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-                child: Text(
-              "Loading...",
-              style: TextStyle(
-                fontFamily: "Montesserat",
-                fontWeight: FontWeight.w700,
-                fontSize: 40.0,
-                fontStyle: FontStyle.italic,
-              ),
-            ));
-          }
-        });
-  }*/
+class _MyAppointmentsState extends State<MyAppointments> {
+  void initState() {
+    super.initState();
+    _loadAppointments();
+  }
 
+/*
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,87 +32,57 @@ class MyAppointments extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              Card(
-                margin: const EdgeInsets.all(10),
-                child: InkWell(
-                  splashColor: Colors.blue.withAlpha(30),
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => ShowAppointment(
-                            user: _user, appointment: appointment1)));
-                  },
-                  child: Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        ListTile(
-                            title: Text(
-                          appointment1.reason,
-                          style: TextStyle(fontSize: 17),
-                        )),
-                        Text(appointment1.dateTimeString),
-                        Text(appointment1.site),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Card(
-                margin: const EdgeInsets.all(10),
-                child: InkWell(
-                  splashColor: Colors.blue.withAlpha(30),
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => ShowAppointment(
-                            user: _user, appointment: appointment2)));
-                  },
-                  child: Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        ListTile(
-                            title: Text(
-                          appointment2.reason,
-                          style: TextStyle(fontSize: 17),
-                        )),
-                        Text(appointment2.dateTimeString),
-                        Text(appointment2.site),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Card(
-                margin: const EdgeInsets.all(10),
-                child: InkWell(
-                  splashColor: Colors.blue.withAlpha(30),
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => ShowAppointment(
-                            user: _user, appointment: appointment3)));
-                  },
-                  child: Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        ListTile(
-                            title: Text(
-                          appointment3.reason,
-                          style: TextStyle(fontSize: 17),
-                        )),
-                        Text(appointment3.dateTimeString),
-                        Text(appointment3.site),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
+              ListView.builder(
+                  itemCount: list.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                        child: Center(
+                      child: Text(
+                        list[index]!.reason,
+                      ),
+                    ));
+                  })
             ],
           ),
         ));
+  }*/
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<dynamic>(
+        future: _loadAppointments(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text(
+              'There was an error :(',
+              style: Theme.of(context).textTheme.headline,
+            );
+          } else if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                      child: Center(
+                    child: Column(children: <Widget>[
+                      Card(
+                        child: Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Column(
+                            children: [
+                              ListTile(
+                                title: Text(snapshot.data![index].toString()),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ]),
+                  ));
+                });
+          } else {
+            return CircularProgressIndicator();
+          }
+        });
   }
 
   _loadAppointments() async {
--- lib/services/appointment_service.dart
+++ lib/services/appointment_service.dart
@@ -3,12 +3,13 @@ import 'dart:async';
 import 'package:app_turnar/domain/appointment.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 
 class AppointmentService {
   final databaseReference = FirebaseDatabase.instance.reference();
 
-  void createData(Appointment _appointment) {
+  Future<void> createData(Appointment _appointment) async {
     var uuid = Uuid();
 
     final appointmentsRef = databaseReference.child("appointments");
@@ -16,29 +17,62 @@ class AppointmentService {
 
     appointmentsRef.child(_uuid).set({
       'user': _appointment.user!.uid,
-      'date': _appointment.date,
-      'time': _appointment.time,
+      'date': _appointment.date.toString(),
+      'time': _appointment.formatTimeOfDay(_appointment.time),
       'site': _appointment.site,
       'reason': _appointment.reason
     });
 
     databaseReference.child("users").child(_appointment.user!.uid);
-    getAllUserAppointments();
   }
 
-  Future<Map<dynamic, dynamic>?> getAllUserAppointments() async {
+  TimeOfDay timeConvert(String normTime) {
+    int hour;
+    int minute;
+    String ampm = normTime.substring(normTime.length - 2);
+    String result = normTime.substring(0, normTime.indexOf(' '));
+    if (ampm == 'AM' && int.parse(result.split(":")[1]) != 12) {
+      hour = int.parse(result.split(':')[0]);
+      if (hour == 12) hour = 0;
+      minute = int.parse(result.split(":")[1]);
+    } else {
+      hour = int.parse(result.split(':')[0]) - 12;
+      if (hour <= 0) {
+        hour = 24 + hour;
+      }
+      minute = int.parse(result.split(":")[1]);
+    }
+    return TimeOfDay(hour: hour, minute: minute);
+  }
+
+  Appointment? convertToAppointment(dynamic appointment) {
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    final User? user = auth.currentUser;
+    if (appointment['user'] == user!.uid) {
+      DateTime date = DateTime.parse(appointment['date']);
+      TimeOfDay time = timeConvert(appointment['time']);
+      return Appointment(
+          user, date, time, appointment['site'], appointment['reason']);
+    }
+  }
+
+  Future<List<Appointment?>> getAllUserAppointments() async {
+    List<Appointment?> list = [];
     final FirebaseAuth auth = FirebaseAuth.instance;
     final User? user = auth.currentUser;
     databaseReference
         .child("appointments")
         .once()
         .then((DataSnapshot snapshot) {
-      Map<dynamic, dynamic>.from(snapshot.value).forEach((key, values) {
-        if (values['user'] == user!.uid) {
-          return snapshot.value;
+      Map<dynamic, dynamic>.from(snapshot.value)
+          .forEach((appointmentID, appointment) {
+        if (appointment['user'] == user!.uid) {
+          Appointment? app = convertToAppointment(appointment);
+          list.add(app);
         }
       });
     });
+    return list;
   }
 
 /*
